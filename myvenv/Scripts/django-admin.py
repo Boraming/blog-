16025bf3d03c54b1b36c19a0185f09e311e77c4f
Boram <boram0713121@naver.com>
2019-07-24 16:07:57 +0900

    add myvenv

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\박보람\공부\2019-1\멋쟁이사자처럼\3.django-3 admin\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
